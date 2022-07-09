0875d1b9afaf51fe6a34477cda268d5190f18267
asili2001 <ahmadasili1928@gmail.com>
2022-07-10 00:09:22 +0200

    better styling
    changed rain background

--- royal-weather/src/components/WeatherCard.jsx
+++ royal-weather/src/components/WeatherCard.jsx
@@ -5,29 +5,31 @@ import ArrowUp from '../assets/symbols/arrow-up.svg';
 const WeatherCard = ({data}) => {
     return(
         <div className={`item ${data.size} ${data.bg}`} >
-            {
-                data.type === 'day' ?
-                    <BookmarkBtn date={data.unixTimeStamp}/>
-                :
-                null
-            }
-            <h1>{data.title}</h1>
-            <p>{data.date}</p>
-            
-            {
-                data.type === "hour" ? <h1>{data.temp}°</h1> : 
+            <div className="wrapper">
+                {
+                    data.type === 'day' ?
+                        <BookmarkBtn date={data.unixTimeStamp}/>
+                    :
+                    null
+                }
+                <h1>{data.title}</h1>
+                <p>{data.date}</p>
                 
-                <div className="hlTemp">
-                    <div>
-                        <img src={ArrowUp} />
-                        <p>{data.hTemp}°</p>
+                {
+                    data.type === "hour" ? <h1>{data.temp}°</h1> : 
+                    
+                    <div className="hlTemp">
+                        <div>
+                            <img src={ArrowUp} />
+                            <p>{data.hTemp}°</p>
+                        </div>
+                        <div>
+                            <img src={ArrowUp} style={{transform : 'rotate(180deg)'}}/>
+                            <p>{data.lTemp}°</p>
+                        </div>
                     </div>
-                    <div>
-                        <img src={ArrowUp} style={{transform : 'rotate(180deg)'}}/>
-                        <p>{data.lTemp}°</p>
-                    </div>
-                </div>
-            }
+                }
+            </div>
         </div>
     )
 }
